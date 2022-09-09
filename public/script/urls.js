0b08ac573cb46abc9417c18e155f4b4c00917174
Joseph <joseph.torres.yz@gmail.com>
2022-09-09 12:19:44 -0500

    Cambio URL sin 198.168.0.8

--- public/script/urls.js
+++ public/script/urls.js
@@ -1,38 +1,38 @@
-const direccion = "192.168.0.8";
+const direccion = "192.168.0.8:3000";
 //let url0  = "http://localhost:3000/api/update/dark-light/";
-const url0  = direccion + ":3000/api/update/dark-light/";
+const url0  = "/api/update/dark-light/";
 
 //let url1 = "http://localhost:3000/api/Productos";
-const url1 = direccion + ":3000/api/Productos";
+const url1 = "/api/Productos";
 //let url2 = "http://localhost:3000/api/Productos/update/";
-const url2 = direccion + ":3000/api/Productos/update/";
+const url2 = "/api/Productos/update/";
 //let url3 = "http://localhost:3000/api/Productos/subtract/";
-const url3 = direccion + ":3000/api/Productos/subtract/";
+const url3 = "/api/Productos/subtract/";
 //let url4 = "http://localhost:3000/api/all/";
-const url4 = direccion + ":3000/api/all/";
+const url4 = "/api/all/";
 //let url5 = "http://localhost:3000/api/Vista_pez/";
-const url5 = direccion + ":3000/api/Vista_pez/";
+const url5 = "/api/Vista_pez/";
 
 //let url6 = "http://localhost:3000/api/ON";
-const url6 = direccion + ":3000/api/ON";
+const url6 = "/api/ON";
 //let url7 = "http://localhost:3000/api/OFF";
-const url7 = direccion + ":3000/api/OFF";
+const url7 = "/api/OFF";
 
 //let url8 = "http://localhost:3000/api/PID/";
-const url8 = direccion + ":3000/api/PID/";
+const url8 = "/api/PID/";
 
 //let url9 = "http://localhost:3000/api/ON/luminocidad";
-const url9 = direccion + ":3000/api/ON/luminocidad";
+const url9 = "/api/ON/luminocidad";
 //let url10 = "http://localhost:3000/api/OFF/luminocidad";
-const url10 = direccion + ":3000/api/OFF/luminocidad";
+const url10 = "/api/OFF/luminocidad";
 
 //let url11 = "http://localhost:3000/api/ON/bomba";
-const url11 = direccion + ":3000/api/ON/bomba";
+const url11 = "/api/ON/bomba";
 //let url12 = "http://localhost:3000/api/OFF/bomba";
-const url12 = direccion + ":3000/api/OFF/bomba";
+const url12 = "/api/OFF/bomba";
 
 //let url13 = "http://localhost:3000/api/Hora-Comida/";
-const url13 = direccion + ":3000/api/Hora-Comida/";
+const url13 = "/api/Hora-Comida/";
 
 /*
 let url4 = "http://localhost:3000/api/Temperaturas/";
